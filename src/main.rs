8cbedf5b89bb11228ca950ac01d3d3da8072d798
unknown <235568@student.pwr.edu.pl>
2021-12-08 18:09:49 +0100

    1b 1248

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,20 @@ fn main() {
     // parse each element (String) to i32
     let vector: Vec<i32> = buf.lines().map(|l| l.unwrap().parse().unwrap()).collect();
 
+    // stores result of sliding window sum
+    let mut sums: Vec<i32> = Vec::new();
+
     // stores result
     let mut sum = 0;
 
+    // fill sums vector which is 2 elements shorter than input vector
+    for i in 0..vector.len() - 2 {
+        sums.push(vector[i] + vector[i + 1] + vector[i + 2])
+    }
+
     // sum++ if current is bigger than previous
-    for i in 1..vector.len() {
-        if vector[i] > vector[i - 1] {
+    for i in 1..sums.len() {
+        if sums[i] > sums[i - 1] {
             sum += 1;
         }
     }
