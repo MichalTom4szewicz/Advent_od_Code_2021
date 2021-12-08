962ca15a4c68a57cf80e744d1fc555e3936eb025
unknown <235568@student.pwr.edu.pl>
2021-12-08 18:09:08 +0100

    1a 1298

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::io::BufRead;
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("input.txt").unwrap();
+
+    let buf = std::io::BufReader::new(file);
+
+    // parse each element (String) to i32
+    let vector: Vec<i32> = buf.lines().map(|l| l.unwrap().parse().unwrap()).collect();
+
+    // stores result
+    let mut sum = 0;
+
+    // sum++ if current is bigger than previous
+    for i in 1..vector.len() {
+        if vector[i] > vector[i - 1] {
+            sum += 1;
+        }
+    }
+
+    println!("{}", sum);
 }
