c1bd28f52c0883951b809d1d54582317a48412b9
unknown <235568@student.pwr.edu.pl>
2021-12-09 21:25:31 +0100

    ???

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,51 @@
 use std::fs::File;
 use std::io::BufRead;
 
-// stack overflow transpose
-fn transpose2<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
-    assert!(!v.is_empty());
-    let len = v[0].len();
-    let mut iters: Vec<_> = v.into_iter().map(|n| n.into_iter()).collect();
-    (0..len)
-        .map(|_| {
-            iters
-                .iter_mut()
-                .map(|n| n.next().unwrap())
-                .collect::<Vec<T>>()
-        })
-        .collect()
+// sum of 2 vectors column-wise
+fn add(a: Vec<u32>, b: &Vec<u32>) -> Vec<u32> {
+    let mut c = vec![0; a.len()];
+    for i in 0..a.len() {
+        c[i] = a[i] + b[i];
+    }
+    c
 }
+
 fn main() {
     let file = File::open("input.txt").unwrap();
 
     let buf = std::io::BufReader::new(file);
 
     // collect input data into 2 dim vector
-    let vector = buf
+    let vectors = buf
         .lines()
-        .map(|l| l.unwrap().chars().collect())
-        .collect::<Vec<Vec<char>>>();
+        .map(|l| {
+            l.unwrap()
+                .chars()
+                .map(|item| item.to_digit(10).unwrap())
+                .collect()
+        })
+        .collect::<Vec<Vec<u32>>>();
 
-    let s_len = vector[0].len();
-    let transposed = transpose2(vector);
+    let mut array = vec![0u32; vectors[0].len()];
+
+    // sum columns
+    for data in &vectors {
+        array = add(array, data)
+    }
 
     let mut gamma: u32 = 0;
     let mut epsilon: u32 = 0;
 
-    let mut i = 0;
-    for line in transposed {
-        let sum: u32 = line
-            .iter()
-            .map(|item| item.to_digit(10).unwrap())
-            .collect::<Vec<u32>>()
-            .iter()
-            .sum();
+    let len = vectors[0].len();
+    let height = vectors.len();
 
-        if 2 * sum > line.len() as u32 {
-            gamma += 2u32.pow((s_len - 1 - i) as u32);
+    // if sum of column value is greater than half of column height then gamma 1 else epsilon 1
+    for (i, num) in array.iter().enumerate() {
+        if 2 * num > height as u32 {
+            gamma += 2u32.pow((len - 1 - i) as u32);
         } else {
-            epsilon += 2u32.pow((s_len - 1 - i) as u32);
+            epsilon += 2u32.pow((len - 1 - i) as u32);
         }
-        i += 1;
     }
 
     println!("{}", epsilon * gamma);
